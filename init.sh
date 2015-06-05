50cb90ac80bdb4bdde7fe820e94a8b6de5ab7b66
yanyalong <yanyalong@gaosiedu.com>
2015-06-05 10:18:14 +0800

    修改脚本

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 curdir=$(cd `dirname $0`;pwd)
 cd $HOME
-mkdir -p .vim/vimfiles/bundle/vundle
-git clone https://github.com/gmarik/Vundle.vim.git .vim/vimfiles/bundle/vundle
+ln -s vimer/vim/ .vim
+ln -s vimer/vimrc/ .vimrc
+#mkdir -p .vim/vimfiles/bundle/vundle
+#git clone https://github.com/gmarik/Vundle.vim.git .vim/vimfiles/bundle/vundle
 
