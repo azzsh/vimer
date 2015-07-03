055fa2b905d6ded08fded896079da0b8346ed1c6
longtest <longtest@azzsh.com>
2015-07-03 12:08:46 +0800

    修改说明文件

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 curdir=$(cd `dirname $0`;pwd)
 cd $HOME
+rm -rf .vim
+rm -rf .vimrc
 mkdir -p  vimer/vim/vimfiles/bundle/vundle
 mkdir -p vimer/vim/view
 git clone https://github.com/gmarik/Vundle.vim.git vimer/vim/vimfiles/bundle/vundle
---
+++ uninit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curdir=$(cd `dirname $0`;pwd)
+cd $HOME
+rm -rf .vim
+rm -rf .vimrc
+rm -rf vimer
+
